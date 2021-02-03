79740a579bc9cbf0a9dd12723e95056dac945a4a
diogoscr <diogoscr@gmail.com>
2021-02-03 18:16:48 -0300

    Product list

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,61 @@
 package application;
 
-import java.io.File;
-import java.util.Scanner;
+import entities.Product;
+
+import java.io.*;
+import java.util.*;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        List<Product> list = new ArrayList<>();
+
         System.out.println("Enter a file path: ");
-        String strPath = sc.nextLine();
+        String sourceFileStr = sc.nextLine();
 
-        File path = new File(strPath);
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
 
-        System.out.println("getName: "+path.getName());
-        System.out.println("getParent: "+path.getParent());
-        System.out.println("getPath: "+path.getPath());
+        boolean mkdir = new File(sourceFolderStr + "\\out").mkdir();
 
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr)) ) {
+
+            String itemCsv = br.readLine();
+
+            while (itemCsv != null) {
+
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+                for (Product item : list) {
+                    bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(targetFileStr + "CREATED!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
         sc.close();
     }
 }
